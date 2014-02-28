338cba9c27ffd8255c29167be91759be6dcec042
Karl Akert <kpakert@gmail.com>
2014-02-28 02:02:44 -0600

    moving more function to separate files, adding .gitignore

--- makefile
+++ makefile
@@ -1,5 +1,7 @@
-shell: shell.o initShell.o
-	gcc -o shell shell.o initShell.o
+shell: shell.o initShell.o parseOptions.o
+	gcc -o shell shell.o initShell.o parseOptions.o
+parseOptions.o: parseOptions.c shell.h
+	gcc -c parseOptions.c
 initShell.o: initShell.c shell.h
 	gcc -c initShell.c	
 shell.o: shell.c shell.h
--- shell.c
+++ shell.c
@@ -25,42 +25,6 @@ int main(int argc, char **argv)
 	}
 }
 
-void parseCommand(int *count, char ***args, char* command)
-{
-	
-	char commandTemp[commandLen];
-	strcpy(commandTemp, command);
-	*count = countArgs(commandTemp);
-	*args = malloc(sizeof(char*) * *count);
-	char* arg;
-	int i = 0;
-	arg = strtok(commandTemp, " \n");
-	while (arg != NULL)
-	{
-		(*args)[i++] = arg;
-		arg = strtok(NULL, " \n");
-	}
-}
-
-int countArgs(char* command)
-{
-	int count = 0;
-	while(1)
-	{
-		while(*command == ' ')
-			command++;
-
-		if (*command == 0 || *command == '\n')
-			break;
-
-		count++;
-
-		while(*command != 0 && *command != ' ')
-			command++;
-	}
-	return count;
-}
-
 void handleCommand(int argc, char **argv)
 {
 	int i;
